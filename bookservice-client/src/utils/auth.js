e18bfe95bc4bc93606c84eac83848a2fcee67ec0
Jimmy Chang <rocmewtwo@gmail.com>
2024-09-12 16:59:04 +0800

    fix issue if user modify token

--- bookservice-client/src/utils/auth.js
+++ bookservice-client/src/utils/auth.js
@@ -14,16 +14,21 @@ export const isTokenExpired = (token) => {
   if (!token) {
     return true;
   }
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  const jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split('')
-      .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
-      .join('')
-  );
-  const jwtPayload = JSON.parse(jsonPayload);
-  const exp = jwtPayload.exp;
-  const currentTime = Math.floor(Date.now() / 1000);
-  return currentTime > exp;
+  try {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+        .join('')
+    );
+    const jwtPayload = JSON.parse(jsonPayload);
+    const exp = jwtPayload.exp;
+    const currentTime = Math.floor(Date.now() / 1000);
+    return currentTime > exp;
+  }
+  catch (err) {
+    return true;
+  }
 };
